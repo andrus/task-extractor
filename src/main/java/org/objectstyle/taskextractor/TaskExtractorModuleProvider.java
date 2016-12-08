5ef9f677b5ef8a56cd142f066cd132d03c126d33
Andrus Adamchik <andrus@objectstyle.com>
2016-12-08 20:20:55 +0300

    new repos

--- src/main/java/org/objectstyle/taskextractor/TaskExtractorModuleProvider.java
+++ src/main/java/org/objectstyle/taskextractor/TaskExtractorModuleProvider.java
@@ -1,6 +1,7 @@
 package org.objectstyle.taskextractor;
 
 import com.google.inject.Module;
+import io.bootique.BQModule;
 import io.bootique.BQModuleProvider;
 
 public class TaskExtractorModuleProvider implements BQModuleProvider {
@@ -8,5 +9,13 @@ public class TaskExtractorModuleProvider implements BQModuleProvider {
     @Override
     public Module module() {
         return new TaskExtractorModule();
+    }
+
+    @Override
+    public BQModule.Builder moduleBuilder() {
+        return BQModuleProvider.super
+                .moduleBuilder()
+                .description("Provides command to extract coding activity from various repositories. " +
+                        "Assists in tracking and such.");
     }
 }
